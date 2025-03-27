02a0a9452437662ba6a15554b080ab35f54c7381
Salavat <Justdrumm@yandex.ru>
2025-03-27 21:32:37 +0300

    refactor: added base and made instalation Spring MVC

---
+++ src/main/java/homework_1/config/WebAppInitializer.java
@@ -0,0 +1,25 @@
+package homework_1.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{
+                AppConfig.class,
+                LiquibaseConfig.class,
+                SwaggerConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"}; // обрабатывать все запросы
+    }
+}
